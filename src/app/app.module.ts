7ec6f759dd701fb7b0db4b60707c443c8701b6d5
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-02 16:10:51 -0500

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modulo de Ruteo
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+//Firebase
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
+//Componentes de mi App
+import { AppComponent } from './app.component'; //Principal
+
+import { NewPlaceComponent } from './components/new-place/new-place.component';
+import { PlaceListComponent } from './components/place-list/place-list.component';
+// import { PlaceMapComponent } from './components/place-map/place-map.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPlaceComponent,
+    PlaceListComponent,
   ],
   imports: [
     BrowserModule,
