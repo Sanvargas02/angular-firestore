398928b1a54221a467abe0e6af7838b5c5c1ad7c
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-02 17:55:10 -0500

    Explicación Interfaces

--- src/app/interfaces/place.interface.ts
+++ src/app/interfaces/place.interface.ts
@@ -1,3 +1,9 @@
+//Interfaz Base para Modelar nuestros datos
+//Podemos extender los datos si es necesario con otra interfaz usando extends o integrar una interfaz y funciones dentro de otra para hacerla más robusta.
+
+// Caso Practico:
+// INTERFACES DE TIPO: ATRACTIVO TURÍSTICO - PRESTADOR TURISTICO - MUNICIPIO.
+// BASADOS EN SUS DEBIDOS FORMULARIOS CON CATEGORÍAS(SERVICIOS COMO DICE EL FORMULARIO) A TENER EN CUENTA.
 export default interface Place {
     id?: string;
     name: string;
@@ -6,3 +12,6 @@ export default interface Place {
     description: string;
     image: string;
 }
+
+//Se puede crear otro tipo de interfaces con el fin de modelar otros tipos de datos distintos
+//Ejemplos sería categorías
