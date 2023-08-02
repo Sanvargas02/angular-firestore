a7f6ce35bbb3486eb6a5395b97eaa23fb0d9ff08
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-02 12:00:09 -0500

    Inicio Proyecto FireStore y Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'gdi-places-app-653a9',
+    appId: '1:799810576216:web:44f008f2a231f323714f2d',
+    storageBucket: 'gdi-places-app-653a9.appspot.com',
+    apiKey: 'AIzaSyAzr7Pj2hfgfl3wY-cH2RWOvSqoJ9CQxao',
+    authDomain: 'gdi-places-app-653a9.firebaseapp.com',
+    messagingSenderId: '799810576216',
+  },};
